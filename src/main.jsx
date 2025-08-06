9dce07970bb65a5071563741e32f9415177dd34c
Waseem_Nasser <waseemjnasser22@gmail.com>
2025-08-06 05:40:03 -0400

    fixing basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='Bank_Project'>
+    <BrowserRouter basename={"Bank_Project"}>
       <App />
     </BrowserRouter>
   </StrictMode>
 )
-
