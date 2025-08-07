8daced181ce19c9ba37dcf0e057caa6ab780681f
molazeeem <m.molazm66@gmail.com>
2025-08-07 16:55:26 +0300

    SectionTitle

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Security from './pages/Security';
 import About from './pages/About';
+import { SectionTitle } from './components/SectionTitle/SectionTitle';
+import { FooterTitle } from './components/FooterTitle/FooterTitle';
+import { SliderSec } from './components/SliderSec/SliderSec';
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
           <Route path="/signUp" element={<SignUp />} />
         </Route>
       </Routes>
+      {/* <SectionTitle titlewhite={"Our"} titlegreen={"Testimonials"} filter={true} paragraph={"Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey"}></SectionTitle>
+      <FooterTitle title={"Start your financial journey with"} paragraph={"Ready to take control of your finances? Join YourBank now, and let us help you achieve your financial goals with our tailored solutions and exceptional customer service"}></FooterTitle> */}
+      <SliderSec></SliderSec>
     </>
   );
 }
